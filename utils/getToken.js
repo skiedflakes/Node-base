66e3a4566faaded41eec9453d7a327cb711a272d
Hitori 'Bocchi' Gotoh <48380074+Anxiety471@users.noreply.github.com>
2025-05-19 15:02:02 +0800

    Update getToken.js
    Added verify token

--- utils/getToken.js
+++ utils/getToken.js
@@ -1,6 +1,19 @@
 const jwt = require('jsonwebtoken');
 const jwtConfig = require('../config/jwt');
 
-module.exports = (userId) => {
-  return jwt.sign({ userId }, jwtConfig.secret, { expiresIn: jwtConfig.expiresIn });
-};
+//module.exports = (userId) => {
+//  return jwt.sign({ userId }, jwtConfig.secret, { expiresIn: jwtConfig.expiresIn });
+//};
+
+const getToken = (data) => {
+  return jwt.sign(data, jwtConfig.secret, { expiresIn: jwtConfig.expiresIn });
+}
+
+const verifyToken = (token) => {
+  return jwt.verify(token,jwtConfig.secret)
+}
+
+
+module.exports=  {
+  getToken,verifyToken
+}
