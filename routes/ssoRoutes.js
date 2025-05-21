17cbccc670466fd04ab687588e662120a36c5e62
Hitori 'Bocchi' Gotoh <48380074+Anxiety471@users.noreply.github.com>
2025-05-21 11:06:26 +0800

    Update ssoRoutes.js

--- routes/ssoRoutes.js
+++ routes/ssoRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { login,getTaxpayer, getBusiness, businessHistory, TaxpayerBusiness } = require('../controllers/ssoController')
+const { login,getTaxpayer, getBusinessandHistory, businessHistory, TaxpayerBusiness, business } = require('../controllers/ssoController')
 const {SSOLogin,SSOVerify} = require('../middleware/ssoMiddleware')
 
 router.post('/login',SSOLogin,login)
 router.post('/taxpayer-business',SSOVerify,TaxpayerBusiness)
-router.post('/get-business',SSOVerify,getBusiness)
+router.post('/business-with-history',SSOVerify,getBusinessandHistory)
+router.post('/business',SSOVerify,business)
 router.post('/business-history',SSOVerify,businessHistory)
 router.get('/taxpayer',SSOVerify,getTaxpayer)
 
